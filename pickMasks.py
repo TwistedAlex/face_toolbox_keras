b3428840af5ee200682a950885386afc217202f3
TwistedAlex <muzihuole@gmail.com>
2022-08-26 12:21:01 -0500

    add background masks generator

--- pickMasks.py
+++ pickMasks.py
@@ -109,6 +109,46 @@ def classify_masks(input_dir, output_dir, output_wrinkles_dir, target_modes, cla
                 cv2.imwrite(output_dir + target_modes[mode_idx] + "\\" + file, output_np[mode_idx])
             cv2.imwrite(output_dir + "wrinkles_others\\" + file, output_np[len_modes])
 
+
+def remove_bg(input_dir, output_wrinkles_dir, target_modes, class_masks_dir, limit):
+    files_dir = os.listdir(input_dir)
+    len_modes = len(target_modes)
+    count = 0
+
+    for file in files_dir:
+        if count < limit:
+            output_np = np.full((len_modes + 1, 1024, 1024), 0, dtype=np.uint8)
+            np_img = cv2.cvtColor(cv2.imread(input_dir + file), cv2.COLOR_BGR2GRAY)
+            numLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(np_img, connectivity=8)
+
+            for idx in range(len(stats)):
+                belong_to_specific_region = False
+                if idx == 0:
+                    continue
+                cur_region = np.full((1024, 1024), 0, dtype=np.uint8)
+                start_p = (int(stats[idx][0]), int(stats[idx][1]))
+                end_p = (int(stats[idx][0] + stats[idx][2]),
+                         int(stats[idx][1] + stats[idx][3]))
+                cur_np = cv2.rectangle(cur_region, start_p, end_p, 255, -1)
+                mask = cur_np == 255
+
+                for mode_idx in range(len_modes):
+                    print(class_masks_dir + target_modes[mode_idx] + "\\"+file)
+                    mode_np = cv2.imread(class_masks_dir + target_modes[mode_idx] + "\\"+file)
+                    intersection = np.logical_and(cur_np, mode_np[:,:,0])
+                    if intersection.any():
+                        # merge cur_np into the desired np, init desired np all black(0),
+                        output_np[mode_idx][mask] = 255
+                        belong_to_specific_region = True
+                if not belong_to_specific_region:
+                    output_np[len_modes][mask] = 255
+
+            count += 1
+            for mode_idx in range(len_modes):
+                pathlib.Path(output_wrinkles_dir).mkdir(parents=True, exist_ok=True)
+                cv2.imwrite(output_wrinkles_dir + "\\" + file, output_np[mode_idx])
+
+
 def main():
     modes = ["waterdrops\\", "wrinkles_others_orig\\", "hair\\", ]
     draft_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\"
@@ -118,21 +158,21 @@ def main():
     resize_merge_dir = output_dir + "resizedmerged\\"
     pathlib.Path(merge_dir).mkdir(parents=True, exist_ok=True)
     pathlib.Path(resize_merge_dir).mkdir(parents=True, exist_ok=True)
-    limit = 300
+    limit = 500
     max_patches = 100
 
-    # for idx in range(len(modes)):
-    #     print(modes[idx])
-    #     pathlib.Path(output_dir + modes[idx]).mkdir(parents=True, exist_ok=True)
-    #     pathlib.Path(input_dir + modes[idx]).mkdir(parents=True, exist_ok=True)
-    #     generate_masks(draft_dir + modes[idx], input_dir + modes[idx], limit)
-    #
-    # wrinkles_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\input\\wrinkles_others_orig\\"
-    # output_wrinkles_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\input\\wrinkles_others\\"
-    # target_modes = ["ear", "mouth", "nose", "eye", "eyebrow", "neck"]
-    # class_masks_dir = "E:\\ResearchData\\MASKS\\s1p05\\"
-    #
-    # classify_masks(wrinkles_dir, input_dir, output_wrinkles_dir, target_modes, class_masks_dir, limit)
+    for idx in range(len(modes)):
+        print(modes[idx])
+        pathlib.Path(output_dir + modes[idx]).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(input_dir + modes[idx]).mkdir(parents=True, exist_ok=True)
+        generate_masks(draft_dir + modes[idx], input_dir + modes[idx], limit)
+
+    wrinkles_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\input\\wrinkles_others_orig\\"
+    output_wrinkles_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\input\\wrinkles_others\\"
+    target_modes = ["ear", "mouth", "nose", "eye", "eyebrow", "neck"]
+    class_masks_dir = "E:\\ResearchData\\MASKS\\s1p05\\"
+
+    classify_masks(wrinkles_dir, input_dir, output_wrinkles_dir, target_modes, class_masks_dir, limit)
 
     all_modes = ["ear", "mouth", "nose", "eye", "eyebrow", "neck", "waterdrops", "wrinkles_others", "hair"]
 
@@ -141,6 +181,13 @@ def main():
     merge_masks([output_dir + mo + "\\" for mo in all_modes], merge_dir, limit)
     # resize_masks(merge_dir, resize_merge_dir, 0.5, limit)
 
+    merged_dir = "E:\\ResearchData\\MASKS\\manual\\s1p05\\output\\merged\\"
+    output_dir="E:\\ResearchData\\MASKS\\manual\\s1p05\\output\\"
+    output_merged_dir="E:\\ResearchData\\MASKS\\manual\\s1p05\\output\\debg_merged\\"
+
+    target_modes=["background"]
+    limit=500
+    remove_bg(merged_dir, output_merged_dir, target_modes, class_masks_dir, limit)
 
 if __name__ == '__main__':
 
